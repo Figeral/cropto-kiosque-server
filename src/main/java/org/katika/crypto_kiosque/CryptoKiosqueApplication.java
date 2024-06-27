37527152f34039eb10d091884041878282982052
Figeral <mouliofitzgerard@gmail.com>
2024-06-27 17:02:21 +0100

    project initialization

---
+++ src/main/java/org/katika/crypto_kiosque/CryptoKiosqueApplication.java
@@ -0,0 +1,13 @@
+package org.katika.crypto_kiosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoKiosqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoKiosqueApplication.class, args);
+	}
+
+}
